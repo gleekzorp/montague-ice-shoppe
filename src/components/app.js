be57ffab5c102c0571c66de96864439125263c3f
alivialiljenquist <5liljenquists@gmail.com>
2019-02-22 12:24:06 -0700

    navbar basic HTML

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import NavigationContainer from './navigation-bar/navigation-container';
+
+import Home from "./home";
+import About from "./about-us";
+import Menu from "./menu";
+import Contact from "./contact-us";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
-      </div>
+                <Router>
+                    <div>
+
+                        <NavigationContainer />
+
+                        <hr/>
+
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route path="/about-us" component={About} />
+                            <Route path="/menu" component={Menu} />
+                            <Route path="/contact-us" component={Contact} />
+                        </Switch>
+                    </div>
+                </Router>
+            </div>
     );
   }
 }
