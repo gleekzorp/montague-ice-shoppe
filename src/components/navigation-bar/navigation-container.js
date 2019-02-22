06a60a3c41cd07de7302681c7150590daa913ffc
alivialiljenquist <5liljenquists@gmail.com>
2019-02-22 15:34:01 -0700

    finished nav

--- src/components/navigation-bar/navigation-container.js
+++ src/components/navigation-bar/navigation-container.js
@@ -1,24 +1,24 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import Logo from './blue-logo.png';
 
 export default class NavigationContainer extends Component {
     render() {
         return (
             <div className="nav-container">
                 <div className="blue-logo">
-                    <img src="blue-logo.png" alt="logo"/>
+                    <img src={Logo} alt="logo"/>
                 </div>
                 <div className="links">
-                    <hr className="line_1" />
                     <div className="nav-link"><NavLink exact to="/">HOME</NavLink></div>
-                    {/* dot */}
+                    
                     <div className="nav-link"><NavLink exact to="/about">ABOUT US</NavLink></div>
-                    {/* dot */}
+                    
                     <div className="nav-link"><NavLink exact to="/menu">MENU</NavLink></div>
-                    {/* dot */}
+                    
                     <div className="nav-link"><NavLink exact to="/contact">CONTACT</NavLink></div>
-                    <hr className="line_2" />
                 </div>
+                <hr />
             </div>
         )
     }
