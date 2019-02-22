f4d4e2f0daa5bc685e5aff385f053e74727d8c3e
alivialiljenquist <5liljenquists@gmail.com>
2019-02-22 13:12:12 -0700

    basic nav styles

--- src/components/app.js
+++ src/components/app.js
@@ -2,30 +2,26 @@ import React, { Component } from 'react';
 import NavigationContainer from './navigation-bar/navigation-container';
 
 import Home from "./home";
-import About from "./about-us";
+import About from "./pages/about";
 import Menu from "./menu/main";
-import Contact from "./contact-us";
+import Contact from "./contact/contact-page";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-                <Router>
-                    <div>
-
-                        <NavigationContainer />
-
-                        <hr/>
-
-                        <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route path="/about-us" component={About} />
-                            <Route path="/menu" component={Menu} />
-                            <Route path="/contact-us" component={Contact} />
-                        </Switch>
-                    </div>
-                </Router>
-            </div>
+          <Router>
+              <div>
+                  <NavigationContainer />
+                  <Switch>
+                      <Route exact path="/" component={Home} />
+                      <Route path="/about-us" component={About} />
+                      <Route path="/menu" component={Menu} />
+                      <Route path="/contact-us" component={Contact} />
+                  </Switch>
+              </div>
+          </Router>
+      </div>
     );
   }
 }
