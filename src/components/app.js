328a53647ccd308ad01278c86071aa81c19307e4
manuelhelaman <manuel.helaman.soto@gmail.com>
2019-02-22 13:13:10 -0700

    basic footer styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import MainMenuPage from './MenuPage/main'
-
+import FooterContainer from './navigation-bar/navigation-container';
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <MainMenuPage />
-      </div>
+      <div className='app-footer'>
+                <Router>
+                    <div>
+
+                        <NavigationContainer />
+
+                        <hr/>
+
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route path="/about-us" component={About} />
+                            <Route path="/menu" component={Menu} />
+                            <Route path="/contact-us" component={Contact} />
+                        </Switch>
+                    </div>
+                </Router>
+            </div>
     );
   }
 }
+
+
+
+
+
+
