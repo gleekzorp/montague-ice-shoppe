b02a5784135a4a5c443ed1bdc584cceca618485b
croberts7 <ctheroberts@gmail.com>
2019-02-22 13:09:47 -0700

    About page and images and video

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,25 +1,45 @@
 import React from 'react';
+import image from '../../../static/assets/images/galen-profile.jpg';
 
 export default function(){
     return(
-        <div className="body">
+        <div className="about-page">
           {/* <Header/> */}
-            <div className="video-stuff">
-                <div className="video-div">kjgijeig</div>
+            <div className="about-module">
+                <div className="video-div">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/uzjB52_gGgk?autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
                 <div className="video-text">
-                    <h1>
-                    I'm not looking for judgement, just a yes or no. Can you assimilate a giraffe? Cause he roped me into this! What, I'm gonna touch it, and you're gonna tell me it's an alien dick or something? You're our boy dawg, don't even trip.
-                    </h1>
+                    <h1>Here is a video about how we make our ice cream</h1>
+                </div>
+            </div>
+            <hr/>
+            <div className="about-module">
+                <div className="about-text">
+                    <h1>Here is the history of our store</h1>
+                    <p>Summer, next time we're hiding in a chlorkian echo nest, can you do me a favour and turn your ringer off?! I just killed my family! 
+                        
+                    I don’t care who they were! They're robots Morty! It's okay to shoot them! They're just robots! Alien Invasion Tomato Monster Mexican Armada Brothers Who Are Just Regular Brothers Running In A Van From An Asteroid And All Sorts Of Things: The Movie.
+                    </p>
+                </div>
+                <div className="company-picture">
+                <img src={image} alt="THE PICTURE OF GALEN SHOULD BE HERE"/>
+                </div>
+            </div>
+            <hr/>
+            <div className="about-module">
+                <div className="company-picture">
+                <img src="https://images.unsplash.com/photo-1532947016889-3b7ab9cd0a13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1535&q=80" alt=""/>
+                </div>
+                <div className="about-text">
+                    <h1>Here is a brief history of ice cream</h1>
+                    <p>Summer, next time we're hiding in a chlorkian echo nest, can you do me a favour and turn your ringer off?! I just killed my family! 
+                        
+                    I don’t care who they were! They're robots Morty! It's okay to shoot them! They're just robots! Alien Invasion Tomato Monster Mexican Armada Brothers Who Are Just Regular Brothers Running In A Van From An Asteroid And All Sorts Of Things: The Movie.
+                    </p>
                 </div>
             </div>
 
-            <div className="about-company">
-                <div className="company-text">
-                    <h2>Summer, next time we're hiding in a chlorkian echo nest, can you do me a favour and turn your ringer off?! I just killed my family! I don’t care who they were! They're robots Morty! It's okay to shoot them! They're just robots! Alien Invasion Tomato Monster Mexican Armada Brothers Who Are Just Regular Brothers Running In A Van From An Asteroid And All Sorts Of Things: The Movie.
-                    </h2>
-                </div>
-                <div className="company-picture"></div>
-            </div>
             {/* //<Footer/> */}
         </div>
 
