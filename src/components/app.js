c8296dd89b3c0ab47a0bbfce57412005c90305af
Giancarlo <mision.marquez1314@gmail.com>
2019-02-22 11:23:46 -0700

    made changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import About from './pages/about.js'
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <About />
       </div>
     );
   }
---
+++ src/components/pages/about.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function(){
+    return(
+        <div className="body">
+          {/* <Header/> */}
+            <div className="video-stuff">
+                <div className="video-div">kjgijeig</div>
+                <div className="video-text">
+                    <h1>
+                    I'm not looking for judgement, just a yes or no. Can you assimilate a giraffe? Cause he roped me into this! What, I'm gonna touch it, and you're gonna tell me it's an alien dick or something? You're our boy dawg, don't even trip.
+                    </h1>
+                </div>
+            </div>
+
+            <div className="about-company">
+                <div className="company-text">
+                    <h2>Summer, next time we're hiding in a chlorkian echo nest, can you do me a favour and turn your ringer off?! I just killed my family! I don’t care who they were! They're robots Morty! It's okay to shoot them! They're just robots! Alien Invasion Tomato Monster Mexican Armada Brothers Who Are Just Regular Brothers Running In A Van From An Asteroid And All Sorts Of Things: The Movie.
+                    </h2>
+                </div>
+                <div className="company-picture"></div>
+            </div>
+            {/* //<Footer/> */}
+        </div>
+
+
+    )
+}
