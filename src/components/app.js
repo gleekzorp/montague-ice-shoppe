79a942da63287d9a1104c52c0d3f3e275259c2d9
gleekzorp <gleek.dan@gmail.com>
2019-02-22 11:28:01 -0700

    We created a home.js and added it to the app.js.  On the home page we put the video (we couldn't find an ice cream video yet) and some basic content.  We added a few styles in the main.scss under .homeWrapper

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import Home from "./home"
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <Home />
+
       </div>
     );
   }
